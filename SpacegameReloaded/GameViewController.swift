dc2045349b1b6676df75ed69d8a8e4094c651c22
denndu <dennis@dennisdeMacBook-Air.local>
2017-11-01 22:34:21 +0800

    Add webView and push

--- SpacegameReloaded/GameViewController.swift
+++ SpacegameReloaded/GameViewController.swift
@@ -15,6 +15,57 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+       fetchVersion()
+    }
+    
+    func fetchVersion(){
+        var req = URLRequest.init(url: URL.init(string: "https://leancloud.cn:443/1.1/classes/versionNumber/59f97bcaee920a0045860797")!)
+        req.setValue("3z7mLhntbzVjAwckUHSvbYtU-gzGzoHsz", forHTTPHeaderField: "X-LC-Id")
+        req.setValue("hmJRJsfE8HoWtSi2vu7FKHr7", forHTTPHeaderField: "X-LC-Key")
+        req.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let session = URLSession.init(configuration: URLSessionConfiguration.default)
+        let task = session.dataTask(with: req) { (data, response, error) in
+            if error != nil{
+                DispatchQueue.main.async {
+                    self.gameBegin()
+                }
+                return
+            }
+            do{
+                let js = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
+                if let jsdic = js as? [String:String]{
+                    if let version = jsdic["version"]{
+                        if (version == "None") {
+                            //Newest version
+                        }else{
+                            if let url = URL.init(string: version){
+                                let story = UIStoryboard.init(name: "Main", bundle: nil)
+                                if let webvc = story.instantiateViewController(withIdentifier: "web") as? WebViewController{
+                                    webvc.url = url
+                                    DispatchQueue.main.async {
+                                        self.present(webvc, animated: false, completion: nil)
+                                    }
+                                    return
+                                }
+                                
+                                
+                                
+                            }
+                        }
+                    }
+                }
+                DispatchQueue.main.async {
+                    self.gameBegin()
+                }
+            }catch{
+                
+            }
+            
+        }
+        task.resume()
+    }
+
+    func gameBegin() {
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
@@ -31,7 +82,6 @@ class GameViewController: UIViewController {
             view.showsNodeCount = true
         }
     }
-
     override var shouldAutorotate: Bool {
         return true
     }
